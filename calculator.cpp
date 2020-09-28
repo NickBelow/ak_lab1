8dabe7207c41b035933a1703d9496f2f95eb8075
NickBelow <nickbelow2000@gmail.com>
2020-09-28 22:58:43 +0300

    15 changes

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,26 @@
 #include "calculator.h"
 
+// This is calculator
+
+//This is ADD func
 int Calculator::Add (double a, double b)
 {
-	return a + b + 0.5;
+	return a + b;
 }
 
+//This is SUB func
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+//This is MUL func
 int Calculator::Mul (double a, double b)
 {
-    return a * b + 0.5;
+    return a * b;
+}
+
+int Calculator::Div (double a, double b)
+{
+    return a * b;
 }
